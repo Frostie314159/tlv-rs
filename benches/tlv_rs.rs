01072dc8a144d717f626186e94d1096f35f5b423
Frostie314159 <iam.an.programmer@gmail.com>
2023-09-01 12:41:48 +0200

    Made TLV more customizable.

--- benches/tlv_rs.rs
+++ benches/tlv_rs.rs
@@ -3,8 +3,9 @@ use bin_utils::{enum_to_int, Read, Write};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use tlv_rs::TLV;
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
 enum TLVType {
+    #[default]
     Three,
     Unknown(u8),
 }
@@ -16,14 +17,16 @@ enum_to_int! {
     TLVType::Three
 }
 
+type OurTLV = TLV<u8, TLVType, u16, false>;
+
 fn criterion_bench(c: &mut Criterion) {
     let bytes = [0x03, 0x05, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55];
     c.bench_function("bench_read_tlv", |b| {
         b.iter(|| {
-            let _ = TLV::<TLVType>::from_bytes(black_box(&mut bytes.iter().copied())).unwrap();
+            let _ = OurTLV::from_bytes(black_box(&mut bytes.iter().copied())).unwrap();
         })
     });
-    let tlv = TLV::<TLVType>::from_bytes(&mut bytes.iter().copied()).unwrap();
+    let tlv = OurTLV::from_bytes(&mut bytes.iter().copied()).unwrap();
     c.bench_function("bench_write_tlv", |b| {
         b.iter(|| {
             let _ = tlv.to_bytes();
--- src/lib.rs
+++ src/lib.rs
@@ -1,96 +1,170 @@
 #![no_std]
-#![feature(more_qualified_paths)]
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
 
 extern crate alloc;
 
-use alloc::borrow::Cow;
+use core::marker::PhantomData;
+
+use alloc::vec::Vec;
 use bin_utils::*;
+use try_take::try_take;
+pub trait RW:
+    for<'a> ReadCtx<&'a Endian>
+    + for<'a> WriteFixedCtx<{ core::mem::size_of::<Self>() }, &'a Endian>
+    + Copy
+    + Default
+where
+    [(); core::mem::size_of::<Self>()]:,
+{
+}
+impl<T> RW for T
+where
+    [(); core::mem::size_of::<Self>()]:,
+    T: for<'a> ReadCtx<&'a Endian>
+        + for<'a> WriteFixedCtx<{ core::mem::size_of::<Self>() }, &'a Endian>
+        + Copy
+        + Default,
+{
+}
 
 #[cfg_attr(feature = "debug", derive(Debug))]
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Clone, Default, PartialEq, Eq)]
 /// A TLV.
-pub struct TLV<'a, TLVType: From<u8> + Into<u8> + Copy> {
+/// 
+/// This has to be constructed with `..Default::default()` as internally there exists a [PhantomData].
+/// The first type parameter is the raw type of the tlv type.
+/// The second is the strongly typed tlv type, which has to implement conversions from and into the raw tlv type.
+/// The third parameter is the type of the length of the TLV.
+/// The last parameter is a constant boolean, which describes if the fields should be encoded using big endian.
+/// 
+/// ```
+/// #![feature(more_qualified_paths)]
+/// use bin_utils::{enum_to_int, Read, Write};
+/// use tlv_rs::TLV;
+/// 
+/// #[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
+/// enum TLVType {
+///     #[default]
+///     Three,
+///     Unknown(u8),
+/// }
+/// enum_to_int! {
+///     u8,
+/// 
+///     TLVType,
+///     
+///     0x03,
+///     TLVType::Three
+/// }
+/// type OurTLV = TLV<u8, TLVType, u16, false>;
+/// 
+/// let bytes = [0x03, 0x05, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55];
+/// 
+/// let tlv = OurTLV::from_bytes(&mut bytes.iter().copied()).unwrap();
+/// assert_eq!(
+///     tlv,
+///     TLV {
+///         tlv_type: TLVType::Three,
+///         tlv_data: [0x11, 0x22, 0x33, 0x44, 0x55].to_vec(),
+///         ..Default::default()
+///     }
+/// );
+/// assert_eq!(tlv.to_bytes(), bytes);
+/// ```
+pub struct TLV<
+    RawTLVType: RW + From<TLVType>,
+    TLVType: Copy,
+    TLVLength: RW + TryFrom<usize> + Into<usize>,
+    const BIG_ENDIAN: bool,
+> where
+    [(); core::mem::size_of::<RawTLVType>()]:,
+    [(); core::mem::size_of::<TLVType>()]:,
+    [(); core::mem::size_of::<TLVLength>()]:,
+{
     pub tlv_type: TLVType,
 
-    pub tlv_data: Cow<'a, [u8]>,
+    #[doc(hidden)]
+    pub _tlv_length: PhantomData<(RawTLVType, TLVLength)>, // Already encoded in the vector.
+
+    pub tlv_data: Vec<u8>,
 }
-impl<TLVType: From<u8> + Into<u8> + Copy> TLV<'_, TLVType> {
+impl<
+        RawTLVType: RW + From<TLVType>,
+        TLVType: Copy,
+        TLVLength: RW + TryFrom<usize> + Into<usize>,
+        const BIG_ENDIAN: bool,
+    > TLV<RawTLVType, TLVType, TLVLength, BIG_ENDIAN>
+where
+    [(); core::mem::size_of::<RawTLVType>()]:,
+    [(); core::mem::size_of::<TLVType>()]:,
+    [(); core::mem::size_of::<TLVLength>()]:,
+{
+    const HEADER_LENGTH: usize =
+        core::mem::size_of::<RawTLVType>() + core::mem::size_of::<TLVLength>();
+
     pub fn iter(&self) -> impl Iterator<Item = u8> + '_ {
-        core::iter::once(self.tlv_type.into()).chain(
-            (self.tlv_data.len() as u16)
-                .to_le_bytes()
-                .into_iter()
-                .chain(self.tlv_data.iter().copied()),
-        )
+        RawTLVType::from(self.tlv_type)
+            .to_bytes(&Self::get_endian())
+            .into_iter()
+            .chain(
+                <TLVLength as TryFrom<usize>>::try_from(self.tlv_data.len())
+                    .map_err(|_| ())
+                    .expect("Data length exceeded upper limit of length type.")
+                    .to_bytes(&Self::get_endian()),
+            )
+            .chain(self.tlv_data.iter().copied())
+    }
+    pub(crate) const fn get_endian() -> Endian {
+        if BIG_ENDIAN {
+            Endian::Big
+        } else {
+            Endian::Little
+        }
     }
 }
 #[cfg(feature = "read")]
-impl<'a, TLVType: From<u8> + Into<u8> + Copy> Read for TLV<'a, TLVType> {
+impl<
+        RawTLVType: RW + From<TLVType>,
+        TLVType: Copy + From<RawTLVType> + Default,
+        TLVLength: RW + TryFrom<usize> + Into<usize>,
+        const BIG_ENDIAN: bool,
+    > Read for TLV<RawTLVType, TLVType, TLVLength, BIG_ENDIAN>
+where
+    [(); core::mem::size_of::<RawTLVType>()]:,
+    [(); core::mem::size_of::<TLVType>()]:,
+    [(); core::mem::size_of::<TLVLength>()]:,
+{
     fn from_bytes(data: &mut impl ExactSizeIterator<Item = u8>) -> Result<Self, ParserError> {
-        let mut header = data.take(3);
-        if header.len() < 3 {
-            return Err(ParserError::TooLittleData(3 - header.len()));
-        }
+        let mut header = try_take(data, Self::HEADER_LENGTH).map_err(ParserError::TooLittleData)?;
 
-        let tlv_type = TLVType::from(header.next().unwrap());
-        let tlv_length = u16::from_bytes(&mut header, Endian::Little)?;
-        if data.len() < tlv_length.into() {
-            return Err(ParserError::TooLittleData(tlv_length as usize - data.len()));
-        }
-        let tlv_data = data.take(tlv_length as usize).collect();
-        Ok(Self { tlv_type, tlv_data })
+        let tlv_type = TLVType::from(RawTLVType::from_bytes(&mut header, &Self::get_endian())?);
+        let tlv_length = TLVLength::from_bytes(&mut header, &Self::get_endian())?;
+
+        let tlv_data = try_take(data, tlv_length.into())
+            .map_err(ParserError::TooLittleData)?
+            .collect();
+
+        Ok(Self {
+            tlv_type,
+            tlv_data,
+            ..Default::default()
+        })
     }
 }
 #[cfg(feature = "write")]
-impl<'a, TLVType: From<u8> + Into<u8> + Copy> Write<'a> for TLV<'a, TLVType> {
-    fn to_bytes(&self) -> Cow<'a, [u8]> {
-        let tlv_type: u8 = self.tlv_type.into();
-        let tlv_length = (self.tlv_data.len() as u16).to_bytes(Endian::Little);
-        core::iter::once(tlv_type)
-            .chain(
-                tlv_length
-                    .iter()
-                    .copied()
-                    .chain(self.tlv_data.iter().copied()),
-            )
-            .collect()
-    }
-}
-#[cfg(test)]
-mod tests {
-    use alloc::borrow::{Cow, ToOwned};
-    use bin_utils::{enum_to_int, Read, Write};
-
-    use crate::TLV;
-
-    #[derive(Copy, Clone, PartialEq, Eq, Debug)]
-    enum TLVType {
-        Three,
-        Unknown(u8),
-    }
-    enum_to_int! {
-        u8,
-        TLVType,
-
-        0x03,
-        TLVType::Three
-    }
-
-    #[test]
-    fn test_tlv() {
-        let bytes = [0x03, 0x05, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55];
-
-        let tlv = TLV::<TLVType>::from_bytes(&mut bytes.iter().copied()).unwrap();
-        assert_eq!(
-            tlv,
-            TLV {
-                tlv_type: TLVType::Three,
-                tlv_data: [0x11, 0x22, 0x33, 0x44, 0x55].as_slice().to_owned().into()
-            }
-        );
-        assert_eq!(
-            tlv.to_bytes(),
-            <&[u8] as Into<Cow<[u8]>>>::into(bytes.as_slice())
-        );
+impl<
+        RawTLVType: RW + From<TLVType>,
+        TLVType: Copy,
+        TLVLength: RW + TryFrom<usize> + Into<usize>,
+        const BIG_ENDIAN: bool,
+    > Write for TLV<RawTLVType, TLVType, TLVLength, BIG_ENDIAN>
+where
+    [(); core::mem::size_of::<RawTLVType>()]:,
+    [(); core::mem::size_of::<TLVType>()]:,
+    [(); core::mem::size_of::<TLVLength>()]:,
+{
+    fn to_bytes(&self) -> alloc::vec::Vec<u8> {
+        self.iter().collect()
     }
 }
