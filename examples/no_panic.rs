2f5bc23fa7fca1d287656a3cda65389e81150285
Frostie314159 <iam.an.programmer@gmail.com>
2023-12-20 18:22:12 +0100

    Fixed no_panic.

--- examples/no_panic.rs
+++ examples/no_panic.rs
@@ -5,15 +5,15 @@ fn main() {
 #[cfg(all(not(debug_assertions), feature = "no_panic"))]
 #[no_panic::no_panic]
 fn main() {
-    use tlv_rs::TLV;
+    use tlv_rs::raw_tlv::RawTLV;
+    use scroll::{Pread, Pwrite};
 
-    type TestTLV<'a> = TLV<'a, u8, u8, u8, &'a [u8]>;
+    type TestTLV<'a> = RawTLV<'a, u8, u8>;
 
-    let bytes = [0x00, 0x01, 0x00];
-    let test_tlv = TestTLV::from_bytes(&bytes, false).unwrap();
-    let mut buf = [0x00; 3];
-    let _ = test_tlv.clone().into_bytes(&mut buf, false);
-    let _ = test_tlv.into_bytes_capped::<3>(false);
+    let bytes = [0x00u8, 0x01, 0x00];
+    let test_tlv = bytes.pread::<TestTLV>(0).unwrap();
+    let mut buf = [0x00u8; 3];
+    buf.pwrite(test_tlv, 0).unwrap();
 }
 #[cfg(all(debug_assertions, not(feature = "no_panic")))]
 fn main() {}
